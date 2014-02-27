7ef124084e47264e9e8b6bcfe3f3043f8ae89500
ovh <oveach@gmail.com>
2014-02-27 23:55:40 +0100

    Improving date and number formatting using locale

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,43 @@
 angular.module('services', [])
 
-.factory('trips', function($filter){
+.factory('dateService', function(){
+    // @see http://aboutcode.net/2013/07/27/json-date-parsing-angularjs.html
+    var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+
+    return {
+        // convert json string to pure Date objects recursively
+        // to be used after loading object from json serialization
+        fromJson: function(input){
+            // Ignore things that aren't objects.
+            if (typeof input !== "object") return input;
+
+            for (var key in input) {
+                if (!input.hasOwnProperty(key)) continue;
+
+                var value = input[key];
+                var match;
+                // Check for string properties which look like dates.
+                if (typeof value === "string" && (match = value.match(regexIso8601))) {
+                    var milliseconds = Date.parse(match[0])
+                    if (!isNaN(milliseconds)) {
+                        input[key] = new Date(milliseconds);
+                    }
+                } else if (typeof value === "object") {
+                    // Recurse into object
+                    this.fromJson(value);
+                }
+            }
+
+            return input;
+        }
+    }
+})
+
+.factory('trips', function($filter, dateService){
     var trips = {};
     // load trips from localstorage
     if (localStorage.trips) {
-        trips = angular.fromJson(localStorage.trips);
+        trips = dateService.fromJson(angular.fromJson(localStorage.trips));
     } else {
         // initial structure if empty
         trips = {
@@ -48,7 +81,7 @@ angular.module('services', [])
     };
 })
 
-.factory('expenseService', function($filter, $rootScope, trips){
+.factory('expenseService', function($filter, $rootScope, trips, dateService){
     return {
         tripId: null,
         expenses: {
@@ -62,7 +95,7 @@ angular.module('services', [])
             if (this.tripId != tripId) {
                 var key = this.getExpenseKey(tripId);
                 if (localStorage[key]) {
-                    this.expenses = angular.fromJson(localStorage[key]);
+                    this.expenses = dateService.fromJson(angular.fromJson(localStorage[key]));
                     this.tripId = tripId;
                 } else {
                     this.tripId = tripId;

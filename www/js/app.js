7a3f85353d7a885e45360764675c92414c3ac413
Olivier Van Hoof <olivier.vanhoof@alterway.fr>
2015-08-04 01:32:55 +0200

    First step to setup material design

--- www/js/app.js
+++ www/js/app.js
@@ -1,44 +1,9 @@
-angular.module('hollybee', ['ui.router', 'ui.bootstrap', 'ngTouch', 'services', 'controllers'])
+angular.module('hollybee', ['ngMaterial'])
 
-.config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise('/trips');
-    $stateProvider
-        .state('trips', {
-            url: '/trips',
-            controller: 'TripsCtrl',
-            templateUrl: 'views/trips.html'
-        })
-        .state('trips_add', {
-            url: '/trips/add',
-            controller: 'TripCtrl',
-            templateUrl: 'views/trip.html'
-        })
-        .state('trips_edit', {
-            url: '/trips/:tripId/edit',
-            controller: 'TripCtrl',
-            templateUrl: 'views/trip.html'
-        })
-        .state('trips_expenses', {
-            url: '/trips/:tripId/expenses',
-            controller: 'ExpensesCtrl',
-            templateUrl: 'views/expenses.html'
-        })
-        .state('trips_expenses_add', {
-            url: '/trips/:tripId/expenses/add',
-            controller: 'ExpenseCtrl',
-            templateUrl: 'views/expense.html'
-        })
-        .state('trips_expenses_edit', {
-            url: '/trips/:tripId/expenses/:expenseId',
-            controller: 'ExpenseCtrl',
-            templateUrl: 'views/expense.html'
-        })
-        .state('setup', {
-            url: '/setup',
-            controller: 'SetupCtrl',
-            templateUrl: 'views/setup.html'
-        })
-        ;
-})
+.controller('AppCtrl', ['$scope', function($scope){
+    $scope.testBtnClick = function(){
+        alert("test button clicked!");
+    };
+}])
 
 ;

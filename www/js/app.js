7a4bbe8ac3dd3df11ebf1c9b371da596a2c8bd3e
oveach <oveach@gmail.com>
2015-08-08 02:03:14 +0200

    Trips list view with demo data

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,18 @@ angular.module('hollybee', ['ngMaterial'])
     $scope.testBtnClick = function(){
         alert("test button clicked!");
     };
+
+    $scope.trips = [{
+        name: "Roma",
+        startDate: new Date(),
+        endDate: new Date(),
+        budget: 500
+    },{
+        name: "Paris",
+        startDate: new Date(),
+        endDate: new Date(),
+        budget: 8000
+    }];
 }])
 
 ;

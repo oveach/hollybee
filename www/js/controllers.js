8b699ee049be0c8c309ba3944b25f826118713ff
oveach <oveach@gmail.com>
2015-08-12 00:49:45 +0200

    Trip view: add expenses list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,6 +16,38 @@ angular.module('controllers', [])
     .then(function(trip){
         $scope.trip = trip;
     });
+
+    $scope.expenses = [{
+        id: 1,
+        label: "Lorem ipsum",
+        amount: 10.50,
+        date: new Date()
+    },{
+        id: 2,
+        label: "Lorem ipsum",
+        amount: 25,
+        date: new Date()
+    },{
+        id: 3,
+        label: "Lorem ipsum",
+        amount: 123.90,
+        date: new Date()
+    },{
+        id: 4,
+        label: "Lorem ipsum",
+        amount: 10.50,
+        date: new Date()
+    },{
+        id: 5,
+        label: "Lorem ipsum",
+        amount: 25,
+        date: new Date()
+    },{
+        id: 6,
+        label: "Lorem ipsum",
+        amount: 123.90,
+        date: new Date()
+    }];
 }])
 
 ;
